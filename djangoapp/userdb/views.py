298dce47a5911d39e9bb307b4e779604c44fc230
Juan Canales <juancanalescpp@gmail.com>
2013-10-08 14:13:56 -0300

    fix a sync

--- djangoapp/userdb/views.py
+++ djangoapp/userdb/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils import simplejson
 from userdb.models import User
-
+from random import randint
 def sync(request):
 	jsonobj = simplejson.loads(request.POST.get('jsondata',''))
 	for item in jsonobj:
-		imgnewurl = "userpics/" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+		imgnewurl = "userpics/" + datetime.now().strftime("%Y%m%d%H%M%S") + "_%d.png" % (randint(10,99))
 		outputimg = open(imgnewurl, "wb")
 		outputimg.write(item["foto"].decode("base64"))
 		outputimg.close()
