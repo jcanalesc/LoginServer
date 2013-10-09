a45867c9f2d3b5b19139aafd8cc5fb39a0cac4ed
Juan Canales <juancanalescpp@gmail.com>
2013-10-09 16:28:11 -0300

    Agregados campos de preferencias de tragos

--- djangoapp/userdb/models.py
+++ djangoapp/userdb/models.py
@@ -5,9 +5,26 @@ from django.db import models
 class User(models.Model):
 	sM = "M"
 	sF = "F"
+	dVodka 		= "vodka"
+	dWhisky 	= "whisky"
+	dPisco		= "pisco"
+	dRon 		= "ron"
+	dCerveza	= "cerveza"
+	dVino		= "vino"
+	dTequila	= "tequila"
+
 	SEX_CHOICES = (
 		(sM, "Masculino"),
 		(sF, "Femenino"),
+	)
+	DRINK_CHOICES = (
+		(dVodka, "Vodka"),
+		(dWhisky, "Whisky"),
+		(dPisco, "Pisco"),
+		(dRon, "Ron"),
+		(dCerveza, "Cerveza"),
+		(dVino, "Vino"),
+		(dTequila, "Tequila"),
 	)
 	rut 	= models.CharField("Rut del usuario", 		max_length=20, primary_key=True)
 	nombre 	= models.CharField("Nombre del usuario", 	max_length=50) 
@@ -15,7 +32,12 @@ class User(models.Model):
 	foto	= models.ImageField("Foto de perfil",	upload_to="userpics")
 	email	= models.EmailField("Direccion de email del usuario", max_length=254)
 	edad	= models.IntegerField("Edad del usuario")
-	sexo	= models.CharField("Sexo del usuario", choices=SEX_CHOICES, default="M", max_length=2)
+	sexo	= models.CharField("Sexo del usuario", choices=SEX_CHOICES, default=sM, max_length=2)
+	direccion = models.CharField("Direccion del usuario", max_length=200)
+	pref1	= models.CharField("Preferencia de trago 1", choices=DRINK_CHOICES, default=dVodka, max_length=15)
+	pref2	= models.CharField("Preferencia de trago 2", choices=DRINK_CHOICES, default=dVodka, max_length=15)
+	pref3	= models.CharField("Preferencia de trago 2", choices=DRINK_CHOICES, default=dVodka, max_length=15)
+
 
 	def __unicode__(self):
 		return "(%s) %s %s" % (self.rut, self.apellido, self.nombre)
--- djangoapp/userdb/views.py
+++ djangoapp/userdb/views.py
@@ -7,18 +7,22 @@ from random import randint
 from datetime import datetime
 
 def sync(request):
-	jsonobj = simplejson.loads(request.POST.get('jsondata',''))
-	print "Received objects: %d" % (len(jsonobj))
-	for item in jsonobj:
-		imgnewurl = "userpics/" + datetime.now().strftime("%Y%m%d%H%M%S") + "_%d.png" % (randint(10,99))
-		outputimg = open(imgnewurl, "wb")
-		outputimg.write(item["foto"].decode("base64"))
-		outputimg.close()
-		del item["foto"]
-		del item["ts"]
-		item["foto"] = imgnewurl
-		item["edad"] = int(item["edad"])
-		u = User(**item)
-		u.save()
-	response = HttpResponse('{"success": true}', content_type="application/json")
+	response = None
+	try:
+		jsonobj = simplejson.loads(request.POST.get('jsondata',''))
+		print "Received objects: %d" % (len(jsonobj))
+		for item in jsonobj:
+			imgnewurl = "userpics/" + datetime.now().strftime("%Y%m%d%H%M%S") + "_%d.png" % (randint(10,99))
+			outputimg = open(imgnewurl, "wb")
+			outputimg.write(item["foto"].decode("base64"))
+			outputimg.close()
+			del item["foto"]
+			del item["ts"]
+			item["foto"] = imgnewurl
+			item["edad"] = int(item["edad"])
+			u = User(**item)
+			u.save()
+			response = HttpResponse('{"success": true}', content_type="application/json")
+	except:
+		response = HttpResponse('{"success": false}', content_type="application/json")
 	return response
