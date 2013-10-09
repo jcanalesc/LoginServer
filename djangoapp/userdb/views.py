c5796ef4d3a7ac41deaabc57a3b76a9f7205061c
Juan Canales <juancanalescpp@gmail.com>
2013-10-09 12:32:33 -0300

    Arreglado el manejo de CORS, agregado middleware. Registro de usuarios completo.

--- djangoapp/userdb/views.py
+++ djangoapp/userdb/views.py
@@ -4,17 +4,20 @@ from django.shortcuts import get_object_or_404
 from django.utils import simplejson
 from userdb.models import User
 from random import randint
+from datetime import datetime
 
 def sync(request):
-	jsonobj = simplejson.loads(request.GET.get('jsondata',''))
+	jsonobj = simplejson.loads(request.POST.get('jsondata',''))
 	for item in jsonobj:
 		imgnewurl = "userpics/" + datetime.now().strftime("%Y%m%d%H%M%S") + "_%d.png" % (randint(10,99))
 		outputimg = open(imgnewurl, "wb")
 		outputimg.write(item["foto"].decode("base64"))
 		outputimg.close()
+		del item["foto"]
+		del item["ts"]
+		item["foto"] = imgnewurl
+		item["edad"] = int(item["edad"])
+		u = User(**item)
+		u.save()
 	response = HttpResponse('{"success": true}', content_type="application/json")
-	response["Access-Control-Allow-Origin"] = "*"
-	response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-	response["Access-Control-Max-Age"] = "1000"  
-	response["Access-Control-Allow-Headers"] = "*"
 	return response
