943d38e29fd70372151a9c34d43e04bcb5be9a84
Juan Canales <juancanalescpp@gmail.com>
2013-10-23 12:12:00 -0300

    Arreglado error 404 cuando no encuentra el username

--- djangoapp/appmanager/views.py
+++ djangoapp/appmanager/views.py
@@ -13,20 +13,24 @@ def index(request):
 def auth(request):
 	username = request.GET.get('user','')
 	passwd	 = request.GET.get('pass', '')
-	uo = get_object_or_404(User, username=username)
-	response_data = {}
-	if uo.password == passwd:
-		# login succesful, send app_id
-		response_data = {
-			'appid': uo.app.id,
-			'host':	 uo.app.host,
-			'success': True
-		}
-	else:
-		response_data = {
-			'success': False
-		}
-	return HttpResponse(simplejson.dumps(response_data), content_type="application/json")
+	response_data = { "success": False }
+	try:
+		uo = User.objects.get(pk=username)
+		if uo.password == passwd:
+			# login succesful, send app_id
+			response_data = {
+				'appid': uo.app.id,
+				'host':	 uo.app.host,
+				'success': True
+			}
+		else:
+			response_data = {
+				'success': False
+			}
+	except:
+		pass
+	finally:
+		return HttpResponse(simplejson.dumps({response_data}), content_type="application/json")
 
 
 
